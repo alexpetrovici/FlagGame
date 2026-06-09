53c5d50c721d4d02f0e480f6da02f4234ef3f188
Alex <seenetoss@gmail.com>
2026-06-09 15:52:52 +0200

    title

--- src/main.py
+++ src/main.py
@@ -23,25 +23,25 @@ def main(page: ft.Page):
     page.title = "Flag Game"
 
     loaded_countries: list[Country] = init_game()
-    page.data = get_random_country(loaded_countries)
+    page.data       = get_random_country(loaded_countries)
     current_country = page.data
 
-    result_text = ft.Text(value="", size=30)
+    result_text     = ft.Text(value="", size=30)
 
-    flag_image = ft.Image(src=f"{current_country.cca3}.png", width=600, height=400,)
+    flag_image      = ft.Image(src=f"{current_country.cca3}.png", width=600, height=400,)
 
-    capital_text = ft.Text(size=30)
-    continent_text = ft.Text(size=30)
+    capital_text    = ft.Text(size=30)
+    continent_text  = ft.Text(size=30)
     population_text = ft.Text(size=30)
 
     def update_country_view():
         current_country = page.data
 
-        flag_image.src = f"{current_country.cca3}.png"
-        capital_text.value = f"Capital: {current_country.capital[0]}"
-        continent_text.value = f"Continent: {current_country.continents[0]}"
+        flag_image.src        = f"{current_country.cca3}.png"
+        capital_text.value    = f"Capital: {current_country.capital[0]}"
+        continent_text.value  = f"Continent: {current_country.continents[0]}"
         population_text.value = f"Population: {format_population(current_country.population)}"
-        result_text.value = ""
+        result_text.value     = ""
 
     def country_guess(e):
         current_country = page.data
